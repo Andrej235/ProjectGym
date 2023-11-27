8571f78b1dec26688680f399b3f35debfbb77927
Andrej235 <andrejnana445@gmail.com>
2023-11-27 22:58:30 +0100

    Done some cleaning and made a new database controller

--- ProjectGym/Controllers/DatabaseController.cs
+++ ProjectGym/Controllers/DatabaseController.cs
@@ -1,137 +1,88 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using ProjectGym.Data;
-//using ProjectGym.DTOs;
-//using ProjectGym.Models;
-//using ProjectGym.Services.Create;
-//using ProjectGym.Services.Mapping;
-//using ProjectGym.Services.Read;
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectGym.Data;
+using ProjectGym.Utilities;
+using System.Text;
 
-//namespace ProjectGym.Controllers
-//{
-//    [Route("api/database")]
-//    [ApiController]
-//    public class DatabaseController(ExerciseContext context,
-//                              IReadService<Exercise> exerciseReadService,
-//                              IReadService<Muscle> muscleReadService,
-//                              IReadService<Equipment> equipmentReadService,
-//                              IReadService<Image> imageReadService,
-//                              IReadService<Note> noteReadService,
-//                              IReadService<Alias> aliasReadService,
-//                              IEntityMapperAsync<Exercise, ExerciseDTO> exerciseMapper,
-//                              IEntityMapperAsync<Muscle, MuscleDTO> muscleMapper,
-//                              IEntityMapperAsync<Equipment, EquipmentDTO> equipmentMapper,
-//                              IEntityMapper<Image, ImageDTO> imageMapper,
-//                              IEntityMapper<Note, NoteDTO> noteMapper,
-//                              IEntityMapper<Alias, ExerciseAliasDTO> aliasMapper,
-//                              ICreateService<Exercise> exerciseCreateService,
-//                              ICreateService<Muscle> muscleCreateService,
-//                              ICreateService<Equipment> equipmentCreateService,
-//                              ICreateService<Image> imageCreateService,
-//                              ICreateService<Note> noteCreateService,
-//                              ICreateService<Alias> aliasCreateService) : ControllerBase
-//    {
-//        [HttpGet]
-//        public async Task<IActionResult> GetAllData()
-//        {
-//            try
-//            {
-//                DatabaseDTO databaseDTO = new()
-//                {
-//                    Exercises = (await exerciseReadService.Get(x => true, 0, -1, "all")).Select(exerciseMapper.Map),
-//                    Muscles = (await muscleReadService.Get(x => true, 0, -1, "all")).Select(muscleMapper.Map),
-//                    Equipment = (await equipmentReadService.Get(x => true, 0, -1, "all")).Select(equipmentMapper.Map),
-//                    Images = (await imageReadService.Get(x => true, 0, -1, "all")).Select(imageMapper.Map),
-//                    Notes = (await noteReadService.Get(x => true, 0, -1, "all")).Select(noteMapper.Map),
-//                    Aliases = (await aliasReadService.Get(x => true, 0, -1, "all")).Select(aliasMapper.Map),
-//                };
-//                return Ok(databaseDTO);
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message + ex.InnerException?.Message);
-//            }
-//        }
+namespace ProjectGym.Controllers
+{
+    [ApiController]
+    [Route("api/database")]
+    public class DatabaseController(ExerciseContext context) : ControllerBase
+    {
+        struct OldNewIdPairs(object oldId, object newId)
+        {
+            public object oldId = oldId;
+            public object newId = newId;
+        }
 
-//        [HttpPut]
-//        public async Task<IActionResult> LoadData([FromBody] DatabaseDTO databaseDTO)
-//        {
-//            try
-//            {
-//                foreach (var entity in databaseDTO.Equipment)
-//                    await equipmentCreateService.Add(await equipmentMapper.Map(entity));
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(DatabaseSerializationService.Serialize(context));
+            }
+            catch (Exception ex)
+            {
+                LogDebugger.LogError(ex);
+                return BadRequest(LogDebugger.GetErrorMessage(ex));
+            }
+        }
 
-//                foreach (var entity in databaseDTO.Muscles)
-//                    await muscleCreateService.Add(await muscleMapper.Map(entity));
+        [HttpPut]
+        public async Task<IActionResult> Load()
+        {
+            try
+            {
+                await context.Database.EnsureCreatedAsync();
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
 
-//                foreach (var entity in databaseDTO.Exercises)
-//                    await exerciseCreateService.Add(await exerciseMapper.Map(entity));
+                StreamReader reader = new(Request.Body, Encoding.UTF8);
+                await DatabaseDeserializationService.LoadDatabaseAsync(context, await reader.ReadToEndAsync(), new KeyValuePair<string, string>("Creator", "User"));
 
-//                foreach (var entity in databaseDTO.Images)
-//                    await imageCreateService.Add(imageMapper.Map(entity));
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                LogDebugger.LogError(ex);
+                return BadRequest(LogDebugger.GetErrorMessage(ex));
+            }
+        }
 
-//                foreach (var entity in databaseDTO.Notes)
-//                    await noteCreateService.Add(noteMapper.Map(entity));
+        [HttpPost]
+        public async Task<IActionResult> CreateDB()
+        {
+            try
+            {
+                await context.Database.EnsureCreatedAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                LogDebugger.LogError(ex);
+                return BadRequest(LogDebugger.GetErrorMessage(ex));
+            }
+        }
 
-//                foreach (var entity in databaseDTO.Aliases)
-//                    await aliasCreateService.Add(aliasMapper.Map(entity));
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDB()
+        {
+            try
+            {
+                await context.Database.EnsureDeletedAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                LogDebugger.LogError(ex);
+                return BadRequest(LogDebugger.GetErrorMessage(ex));
+            }
+        }
 
-//                return Ok();
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message + ex.InnerException?.Message);
-//            }
-//        }
-
-//        [HttpPost]
-//        public async Task<IActionResult> CreateDB()
-//        {
-//            try
-//            {
-//                await context.Database.EnsureCreatedAsync();
-//                return Ok();
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message + ex.InnerException?.Message);
-//            }
-//        }
-
-//        [HttpDelete]
-//        public async Task<IActionResult> DeleteDB()
-//        {
-//            try
-//            {
-//                await context.Database.EnsureDeletedAsync();
-//                return Ok();
-//            }
-//            catch (Exception ex)
-//            {
-//                return BadRequest(ex.Message + ex.InnerException?.Message);
-//            }
-//        }
-
-//        public class DatabaseDTO
-//        {
-//            public IEnumerable<ExerciseDTO> Exercises { get; set; } = new List<ExerciseDTO>();
-//            public IEnumerable<MuscleDTO> Muscles { get; set; } = new List<MuscleDTO>();
-//            public IEnumerable<EquipmentDTO> Equipment { get; set; } = new List<EquipmentDTO>();
-//            //public IEnumerable<ExerciseComment> ExerciseComments { get; set; }
-//            public IEnumerable<ImageDTO> Images { get; set; } = new List<ImageDTO>();
-//            //public IEnumerable<ExerciseVideo> ExerciseVideos { get; set; }
-//            public IEnumerable<NoteDTO> Notes { get; set; } = new List<NoteDTO>();
-//            public IEnumerable<ExerciseAliasDTO> Aliases { get; set; } = new List<ExerciseAliasDTO>();
-
-//            /*            public IEnumerable<User> Users { get; set; }
-//                        public IEnumerable<Client> Clients { get; set; }
-//                        public IEnumerable<CommentUserUpvote> CommentUserUpvotes { get; set; }
-//                        public IEnumerable<CommentUserDownvote> CommentUserDownvotes { get; set; }
-//                        public IEnumerable<UserExerciseBookmark> UserExerciseBookmarks { get; set; }
-//                        public IEnumerable<Set> Sets { get; set; }
-//                        public IEnumerable<Superset> Supersets { get; set; }
-//                        public IEnumerable<UserExerciseWeight> UserExerciseWeights { get; set; }
-//                        public IEnumerable<Workout> Workouts { get; set; }
-//                        public IEnumerable<WorkoutSet> WorkoutSets { get; set; }*/
-//        }
-//    }
-//}
+        public class StringDTO
+        {
+            public string Value { get; set; } = "";
+        }
+    }
+}
--- ProjectGym/Utilities/DatabaseDeserializationService.cs
+++ ProjectGym/Utilities/DatabaseDeserializationService.cs
@@ -2,7 +2,6 @@
 using ProjectGym.Exceptions;
 using System.Dynamic;
 using System.Reflection;
-using System.Text;
 using System.Text.Json;
 
 namespace ProjectGym.Utilities
@@ -178,36 +177,3 @@ namespace ProjectGym.Utilities
         }
     }
 }
-
-/*        static bool IsNullable(Type type)
-        {
-            if (!type.IsValueType) return true; // Reference type
-            if (Nullable.GetUnderlyingType(type) != null) return true; // Nullable value type
-            return false; // Non-nullable value type
-        }
-
-        static void CheckAllNonNullableProperties(object entity)
-        {
-            foreach (var property in entity.GetType().GetProperties().Where(x => IsNullable(x.PropertyType)))
-            {
-                if (property.GetValue(entity) == null)
-                {
-                    if (property.PropertyType.IsValueType)
-                    {
-                        property.SetValue(entity, Activator.CreateInstance(property.PropertyType));
-                    }
-                    else
-                    {
-                        var defaultConstructor = property.PropertyType.GetConstructor(Type.EmptyTypes);
-                        if (defaultConstructor != null)
-                        {
-                            property.SetValue(entity, Activator.CreateInstance(property.PropertyType));
-                        }
-                        else
-                        {
-                            throw new InvalidOperationException($"Cannot create an instance of type {property.PropertyType.Name}. Consider providing a default constructor or handle this case differently.");
-                        }
-                    }
-                }
-            }
-        }*/
