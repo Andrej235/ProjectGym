1bd0ab8f5b38e016153ce112fd933477fec6f710
Andrej Nenadić <andrejnana445@gmail.com>
2023-12-23 01:02:24 +0100

    Started working on the started set clock

--- AppProjectGym/Pages/StartedWorkoutPage.xaml.cs
+++ AppProjectGym/Pages/StartedWorkoutPage.xaml.cs
@@ -183,4 +183,97 @@ public partial class StartedWorkoutPage : ContentPage, IQueryAttributable
             LogDebugger.LogError(ex);
         }
     }
+
+    private bool isDoingASet = false;
+    private void OnInnerCircleClicked(object sender, EventArgs e)
+    {
+        isDoingASet = !isDoingASet;
+
+        if (isDoingASet)
+            PlayClockAnimation();
+    }
+
+    private async void PlayClockAnimation()
+    {
+        Vector2 vector = new(0, -1);
+        vector.Normalize();
+
+        do
+        {
+            await innerCircle.TranslateTo(vector.X * 75, vector.Y * 75, 10);
+            vector.Rotate(3.6f);
+        } while (isDoingASet);
+
+        EndClockAnimation();
+    }
+
+    private async void EndClockAnimation()
+    {
+        Vector2 vector = new((float)innerCircle.TranslationX, (float)innerCircle.TranslationY);
+        vector.Normalize();
+
+        var currentAngle = vector.GetRotationAngle();
+        while (Math.Abs(currentAngle - 270) > 3.6f)
+        {
+            await innerCircle.TranslateTo(vector.X * 75, vector.Y * 75, 1);
+            vector.Rotate(3.6f);
+            currentAngle += 3.6f;
+        }
+
+        await innerCircle.TranslateTo(0, -75, 1);
+    }
+
+    public class Vector2(float x, float y)
+    {
+        public float X { get; private set; } = x;
+        public float Y { get; private set; } = y;
+
+        public void Normalize()
+        {
+            float length = (float)Math.Sqrt(X * X + Y * Y);
+
+            if (length != 0)
+            {
+                X /= length;
+                Y /= length;
+            }
+        }
+
+        public void Rotate(float degrees)
+        {
+            float radians = (float)(degrees * Math.PI / 180.0);
+            float newX = X * (float)Math.Cos(radians) - Y * (float)Math.Sin(radians);
+            float newY = X * (float)Math.Sin(radians) + Y * (float)Math.Cos(radians);
+
+            X = newX;
+            Y = newY;
+        }
+
+        public static float GetRotationAngle(Vector2 vector1, Vector2 vector2)
+        {
+            float dotProduct = vector1.X * vector2.X + vector1.Y * vector2.Y;
+            float magnitudeProduct = vector1.Magnitude * vector2.Magnitude;
+
+            if (magnitudeProduct == 0)
+                throw new InvalidOperationException("Cannot calculate angle with zero magnitude vectors.");
+
+            float cosTheta = dotProduct / magnitudeProduct;
+            float thetaRadians = (float)Math.Acos(cosTheta);
+            float thetaDegrees = (float)(thetaRadians * 180.0 / Math.PI);
+
+            return thetaDegrees;
+        }
+        public float GetRotationAngle(Vector2 vector2) => GetRotationAngle(this, vector2);
+        public float GetRotationAngle()
+        {
+            var vector2 = new Vector2(0, -1);
+            vector2.Normalize();
+
+            return GetRotationAngle(this, vector2);
+        }
+
+        public float Magnitude => (float)Math.Sqrt(X * X + Y * Y);
+
+        public override string ToString() => $"({X}, {Y})";
+    }
 }
